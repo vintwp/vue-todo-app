c5791239440defecba3a50361b5d81ba3c2fa0e5
unknown <poroh1991ukr@gmail.com>
2026-07-31 15:55:01 +0300

    fixed viteConfig

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/vue-todo-app/'
+  base: '/vue-todo-app/',
   plugins: [
     vue(),
     vueDevTools(),
